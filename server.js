f6710c67c72bf31dd951c5550a86da78ae361169
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-03-21 17:36:43 -0400

    promises now use async functions

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 // require dependancies
-// const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
-// const cTable = require('console.table');
+
 // require modules
 const { Input, List } = require('./lib/prompts');
-const db = require('./lib/db');
-
-// // initiate mysql2
-// const _db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'rootroot',
-//     database: 'findme_db'
-//   },
-// );
+const dbQuery = require('./lib/db');
 
 // UI functionality
 const menu = [
@@ -27,105 +16,37 @@ const menu = [
   'update an employee role',
   'exit'
 ];
+
 const MenuPrompt = [new List('menu', 'Main Menu', menu)];
 
-const promptMenu = () => inquirer.prompt(MenuPrompt);
-  // .then(db.function(answers) {}); <-- put in main function
-
-// // Database functionality
-// const db = (answers) => {
-
-//   if (answers.menu !== 'exit') {
-//     const connParams = {
-//       host: 'localhost',
-//       user: 'root',
-//       password: 'rootroot',
-//       database: 'findme_db'
-//     };
-
-//     if (answers.menu == 'view all departments') {
-//       // initiate mysql2 promise
-//       mysql.createConnection(connParams)
-//       .then(conn => conn.query('SELECT * FROM department'))
-//       .then(([rows, fields]) => {
-//         console.table(rows);
-//         init();
-//       });
-//     }
-//     else if (answers.menu == 'view all roles') {
-//       // initiate mysql2 promise
-//       mysql.createConnection(connParams)
-//       .then(conn => conn.query(''))
-//       .then(([rows, fields]) => {
-//         console.table(rows);
-//         init();
-//       });
-//     }
-//     else if (answers.menu == 'view all employees') {
-//        // initiate mysql2 promise
-//        mysql.createConnection(connParams)
-//        .then(conn => conn.query(`
-//           SELECT e.id, e.first_name, e.last_name, r.title, e.manager_id, r.salary, d.name department 
-//           FROM employee e 
-//           LEFT JOIN role r ON e.role_id = r.id 
-//           LEFT JOIN department d ON r.department_id = d.id
-//           `))
-//        .then(([rows, fields]) => {
-//          console.table(rows);
-//          init();
-//        });
-//     }
-//     else if (answers.menu == 'add a department') {
-//        // initiate mysql2 promise
-//        mysql.createConnection(connParams)
-//        .then(conn => conn.query(''))
-//        .then(([rows, fields]) => {
-//          console.table(rows);
-//          init();
-//        });
-//     }
-//     else if (answers.menu == 'add a role') {
-//        // initiate mysql2 promise
-//        mysql.createConnection(connParams)
-//        .then(conn => conn.query(''))
-//        .then(([rows, fields]) => {
-//          console.table(rows);
-//          init();
-//        });
-//     }
-//     else if (answers.menu == 'add an employee') {
-//        // initiate mysql2 promise
-//        mysql.createConnection(connParams)
-//        .then(conn => conn.query(''))
-//        .then(([rows, fields]) => {
-//          console.table(rows);
-//          init();
-//        });
-//     }
-//     else if (answers.menu == 'update an employee role') {
-//        // initiate mysql2 promise
-//        mysql.createConnection(connParams)
-//        .then(conn => conn.query(''))
-//        .then(([rows, fields]) => {
-//          console.table(rows);
-//          init();
-//        });
-//     };
-//   }
-// }
-
 // Main functionality 
-const init = () => {
-  inquirer.prompt(MenuPrompt)
-  .then(answers => {
-    // console.log(answers);
-    if (answers.menu !== 'exit') {
-      db(answers, init);
+async function init() {
+  // inquirer.prompt(MenuPrompt)
+  // .then(answers => {
+  //   // console.log(answers);
+  //   //TODO CONSTRUCT SWITCH CASE TO HANDLE ANSWERS
+  //   if (answers.menu !== 'exit') {
+  //     db(answers, init);
+  //   } else {
+  //     // process.exit(0);
+  //   }
+  // })
+  const uiSel = await inquirer.prompt(MenuPrompt);
+  
+  switch (uiSel.menu) {
+    case 'view all departments':
+      dbQuery('SELECT * FROM department', init);
       // init();
-    } else {
-      return;
-    }
-  })
+      break;
+    // case 'view all roles':
+    // case 'view all employees':
+    // case 'add a department':
+    // case 'add a role':
+    // case 'add an employee':
+    // case 'update an employee role':
+    case 'exit':
+      process.exit(0);
+  }
 };
 
 init();
