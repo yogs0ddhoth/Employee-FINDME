f742ffdcde9b1a67134ed5789de2c5af004a262c
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-03-21 23:01:38 -0400

    view options functional

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 // require modules
 const { Input, List } = require('./lib/prompts');
 const dbQuery = require('./lib/db');
+const Prompt = require('inquirer/lib/prompts/base');
 
 // UI functionality
 const menu = [
@@ -17,33 +18,39 @@ const menu = [
   'exit'
 ];
 
-const MenuPrompt = [new List('menu', 'Main Menu', menu)];
+const MenuPrompt = [
+  new List('menu', 'Main Menu', menu),
+  new Input('name', 'Enter Department Name:', answers => answers.menu == 'add a department')
+  // new Input('add_role', '', answers => answers.menu == 'add a role'),
+  // new Input('add_emp', '', answers => answers.menu == 'add an employee'),
+];
 
 // Main functionality 
 async function init() {
-  // inquirer.prompt(MenuPrompt)
-  // .then(answers => {
-  //   // console.log(answers);
-  //   //TODO CONSTRUCT SWITCH CASE TO HANDLE ANSWERS
-  //   if (answers.menu !== 'exit') {
-  //     db(answers, init);
-  //   } else {
-  //     // process.exit(0);
-  //   }
-  // })
   const uiSel = await inquirer.prompt(MenuPrompt);
-  
+  console.log(uiSel);
   switch (uiSel.menu) {
     case 'view all departments':
-      dbQuery('SELECT * FROM department', init);
-      // init();
+      dbQuery(init, 'SELECT * FROM departments');
       break;
-    // case 'view all roles':
-    // case 'view all employees':
-    // case 'add a department':
-    // case 'add a role':
-    // case 'add an employee':
-    // case 'update an employee role':
+    case 'view all roles':
+      dbQuery(init, 'SELECT * FROM roles');
+      break;
+    case 'view all employees':
+      dbQuery(init, 'SELECT * FROM employees');
+      break;
+    case 'add a department':
+      dbQuery(init, '');
+      break;
+    case 'add a role':
+      dbQuery(init, '');
+      break;
+    case 'add an employee':
+      dbQuery(init, '');
+      break;
+    case 'update an employee role':
+      dbQuery(init, '');
+      break;
     case 'exit':
       process.exit(0);
   }
@@ -53,8 +60,8 @@ init();
 // default joined tables
 // `
 // SELECT e.id, e.first_name, e.last_name, r.title, e.manager_id, r.salary, d.name department 
-// FROM employee e 
-// LEFT JOIN role r ON e.role_id = r.id 
-// LEFT JOIN department d ON r.department_id = d.id;
+// FROM employees e 
+// LEFT JOIN roles r ON e.role_id = r.id 
+// LEFT JOIN departments d ON r.department_id = d.id;
 // `
 module.exports = init;
